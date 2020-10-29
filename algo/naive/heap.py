08d3e5dc5c83bbf3c78f6cd2a584e5fd42618492
Stas Ukolov <ukoloff@gmail.com>
2020-10-29 13:00:39 +0500

    Implement Heap sort via stdlib

---
+++ algo/naive/heap.py
@@ -0,0 +1,11 @@
+#
+# Non-optimized Heap sort
+#
+import heapq
+
+
+def sort(array):
+    heap = array[:]
+    heapq.heapify(heap)
+    for i in range(len(array)):
+        array[i] = heapq.heappop(heap)

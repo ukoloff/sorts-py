f488a56569bbf72dd766ffac0a82299503e7e0e4
Stas Ukolov <ukoloff@gmail.com>
2020-11-02 18:03:16 +0500

    Measure one algorithm

--- tools/ticks.py
+++ tools/ticks.py
@@ -5,7 +5,7 @@ from .samples import of
 
 def run():
     fns = enum()
-    print(batch_times(fns['bubble'].sort))
+    print(batch_times(fns['std'].sort))
 
 def validate(fn, samples):
   for data in samples:
@@ -16,25 +16,32 @@ def validate(fn, samples):
     assert(a == b)
 
 def single_time(fn, samples, cycles=10):
-  start = time.time()
+  start = time.process_time()
   for data in samples:
     for i in range(cycles):
       a = data[:]
       fn(a)
-  stop = time.time()
+  stop = time.process_time()
   return (stop - start) / cycles / len(samples)
 
-def batch_times(fn, max=30):
+def batch_times(fn, max=60):
   start = time.time()
   result = {}
   size = 1
+  shown = False
   while True:
     data = of(size)
+    validate(fn, data)
     result[size] = single_time(fn, data)
     now = time.time()
+    elapsed = round(now - start)
+    if elapsed != shown:
+      print("\r" , fn.__module__, "\t", elapsed, end="", sep='')
+      shown = elapsed
     if now - start > max:
       break
     size *= 2
+  print()
   return result
 
 run()
